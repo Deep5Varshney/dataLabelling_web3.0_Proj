e1d7c085ff2041d19032e25413b52d1ffcacc0cc
Deep5Varshney <137312194+Deep5Varshney@users.noreply.github.com>
2025-03-16 18:01:23 +0530

    added cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import userRouter from "./Routers/user";
 import workerRouter from "./Routers/worker";
+import cors from "cors";
 const app = express();
+app.use(cors());
 app.use(express.json());
 export const JWT_SECRET = "dhquwey982137";
 
