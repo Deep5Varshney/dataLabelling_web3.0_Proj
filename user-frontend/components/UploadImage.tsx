253d311a3025d8cf76626dca98c8bb943e6dc12f
Deep5Varshney <137312194+Deep5Varshney@users.noreply.github.com>
2025-03-17 14:53:31 +0530

    started the frontend of user

--- user-frontend/components/UploadImage.tsx
+++ user-frontend/components/UploadImage.tsx
@@ -13,7 +13,7 @@ export function UploadImage({ onImageAdded, image }: {
         setUploading(true);
         try {
             const file = e.target.files[0];
-            const response = await axios.get(`${BACKEND_URL}/v1/user/presignedUrl`, {
+            const response = await axios.get(`${BACKEND_URL}/v1/user/presignedurl`, {
                 headers: {
                     "Authorization": localStorage.getItem("token")
                 }
@@ -49,7 +49,7 @@ export function UploadImage({ onImageAdded, image }: {
                     <div className="h-full flex justify-center w-full pt-16 text-4xl">
                     {uploading ? <div className="text-sm">Loading...</div> : <>
                         +
-                        <input className="w-full h-full bg-red-400 w-40 h-40" type="file" style={{position: "absolute", opacity: 0, top: 0, left: 0, bottom: 0, right: 0, width: "100%", height: "100%"}} onChange={onFileSelect} />
+                        <input className="w-full h-full bg-red-400" type="file" style={{position: "absolute", opacity: 0, top: 0, left: 0, bottom: 0, right: 0, width: "100%", height: "100%"}} onChange={onFileSelect} />
                     </>}
                 </div>
             </div>
--- user-frontend/utils/index.ts
+++ user-frontend/utils/index.ts
@@ -1,3 +1,3 @@
 
 export const BACKEND_URL = "http://localhost:3000";
-export const CLOUDFRONT_URL = "https://d2szwvl7yo497w.cloudfront.net"
+export const CLOUDFRONT_URL = "https://dubjkk998gd7c.cloudfront.net"
