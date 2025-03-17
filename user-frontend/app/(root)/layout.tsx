dcfc836f2eb9c3e6d5ebec8d25a0ed50f9144387
Deep5Varshney <137312194+Deep5Varshney@users.noreply.github.com>
2025-03-17 21:22:35 +0530

    init

--- user-frontend/app/(root)/layout.tsx
+++ user-frontend/app/(root)/layout.tsx
@@ -1,30 +1,37 @@
 "use client";
+import React, { FC, useMemo } from 'react';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import {
+    WalletModalProvider
+} from '@solana/wallet-adapter-react-ui';
+import { clusterApiUrl } from '@solana/web3.js';
 
-import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
-import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
-import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
-import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
-import { clusterApiUrl } from "@solana/web3.js";
-import { ReactNode, useMemo } from "react";
+// Default styles that can be overridden by your app
+require('@solana/wallet-adapter-react-ui/styles.css');
 
-require("@solana/wallet-adapter-react-ui/styles.css");
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const network = WalletAdapterNetwork.Mainnet;
 
-interface ProvidersProps {
-    children: ReactNode;
-}
+  // You can also provide a custom RPC endpoint.
+  const endpoint = "your_rpc_url";
 
-const Providers = ({ children }: ProvidersProps) => {
-    const network = WalletAdapterNetwork.Devnet; // Change to 'MainnetBeta' if needed
-    const endpoint = useMemo(() => clusterApiUrl(network), [network]);
-    const wallets = useMemo(() => [new PhantomWalletAdapter()], []);
+  const wallets = useMemo(
+      () => [],
+      [network]
+  );
 
     return (
-        <ConnectionProvider endpoint={endpoint}>
-            <WalletProvider wallets={wallets} autoConnect>
-                <WalletModalProvider>{children}</WalletModalProvider>
-            </WalletProvider>
-        </ConnectionProvider>
-    );
-};
-
-export default Providers;
+    <ConnectionProvider endpoint={endpoint}>
+        <WalletProvider wallets={wallets} autoConnect>
+            <WalletModalProvider>
+                {children}
+            </WalletModalProvider>
+        </WalletProvider>
+    </ConnectionProvider>
+  );
+}
--- user-frontend/components/Upload.tsx
+++ user-frontend/components/Upload.tsx
@@ -64,7 +64,7 @@ export const Upload = () => {
                 setTitle(e.target.value);
             }} type="text" id="first_name" className="ml-4 mt-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="What is your task?" required />
 
-            <label className="pl-4 block mt-8 text-md font-medium text-black">Add Images</label>
+            <label className="pl-4 block mt-8 text-md font-medium  text-black">Add Images</label>
             <div className="flex justify-center pt-4 max-w-screen-lg">
                 {images.map(image => <UploadImage image={image} onImageAdded={(imageUrl) => {
                     setImages(i => [...i, imageUrl]);
